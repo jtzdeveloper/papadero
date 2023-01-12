2844821ddaf692b1954aa3e945f6916c733cb9ac
Johnny Torres <jony.tz@gmail.com>
2023-01-12 02:03:15 -0300

    gh-pages added

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/papadero/',
-  define: {
+ /*  define: {
     "global": {},
-  },
-  server: {
+  }, */
+ /*  server: {
     host: true
-  }
+  } */
 })
